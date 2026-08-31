9ee98d3271e2ff98bf596987214c28ca43d66461
Seeedurk <sedrikpunzalan@gmail.com>
2026-08-30 19:31:51 -0500

    Added requirements.txt and changed hardcoded socket

--- Client/Components/Socket.js
+++ Client/Components/Socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000");
-export default socket;  
+const socket = io(process.env.REACT_APP_BACKEND_URL || "http://localhost:5000");
+export default socket;
